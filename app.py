13eba82f4c6d54f5f47a38cdc052646872700284
Lukas Hofstetter <lhofstetter@users.noreply.huggingface.co>
2024-03-13 20:09:20 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ import streamlit as st
 import os
 st.title('Hello Urs')
 st.text(os.getcwd())
-st.image('/home/user/app/2022_04_09%2C%2002_51_32_757135.png')
+st.image('/home/user/app/2022_04_09, 02_51_32_757135.png')
