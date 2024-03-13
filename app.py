227564e053a4fb924d6d9decc7f4bf719804453a
Lukas Hofstetter <lhofstetter@users.noreply.huggingface.co>
2024-03-13 17:13:45 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
 import streamlit as st
+import os
 st.title('Hello Urs')
+st.text(os.getcwd())
 st.image('https://huggingface.co/spaces/swissrail/PantoScanner/blob/main/2022_04_09%2C%2002_51_32_757135.png')
