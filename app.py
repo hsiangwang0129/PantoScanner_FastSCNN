c7a2f0c4b2bfd6916a3cc276830049b574131f81
Lukas Hofstetter <lhofstetter@users.noreply.huggingface.co>
2024-03-13 20:16:08 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import os
 import glob
-
+import cv2
 
 st.title('Hello Urs')
 st.text(os.getcwd())
 st.text(glob.glob(f'{os.getcwd()}/*.png')[0])
-st.image(glob.glob(f'{os.getcwd()}/*.png')[0])
+img_array = cv2.imread(glob.glob(f'{os.getcwd()}/*.png')[0])
+st.image(img_array)
