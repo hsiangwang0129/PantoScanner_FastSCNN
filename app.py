f2a8508f79f07ba2f3bfd5248a83b3a3c446f4d7
Lukas Hofstetter <lhofstetter@users.noreply.huggingface.co>
2024-03-13 20:57:01 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,24 @@ import os
 import glob
 import cv2
 
-st.title('Hello Urs')
-st.text(os.getcwd())
-st.text(glob.glob(f'{os.getcwd()}/*.png')[0])
-img_array = cv2.imread(glob.glob(f'{os.getcwd()}/*.png')[0])
-st.image(img_array)
+image_emoji = '📷'
+model_emoji = '⚙️'
+profile_emoji = '📈'
+st.title('PantoScan')
+
+tab1, tab2, tab3 = st.tabs([f'{image_emoji} Image', f'{model_emoji} Mask', f'{profile_emoji} Measurement'])
+
+with tab1:
+   st.header(f'Source Image')
+   img_array = cv2.imread(glob.glob(f'{os.getcwd()}/*.png')[0])
+    st.image(img_array)
+
+with tab2:
+   st.header(f'Model Output')
+   #st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+
+with tab3:
+   st.header(f'Profile Height')
+   #st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+
+
