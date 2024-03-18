c75618b9bec08706a867ecfd2cb130ffa717a40b
Urs Gehrig <ursgehrig@users.noreply.huggingface.co>
2024-03-18 17:22:00 +0000

    Update object_detection_services.py

--- object_detection_services.py
+++ object_detection_services.py
@@ -1,3 +1,29 @@
+# -----------------------------------------------------------------------------
+#
+# This file is part of the PantoScanner distribution on: 
+# https://huggingface.co/spaces/swissrail/PantoScanner
+#
+# PantoScanner - Analytics and measurement capability for technical objects.
+# Copyright (C) 2017-2024 Schweizerische Bundesbahnen SBB
+#
+# Authors (C) 2024 L. Hofstetter (lukas.hofstetter@sbb.ch)
+# Authors (C) 2017 U. Gehrig (urs.gehrig@sbb.ch)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# -----------------------------------------------------------------------------
+
 import os
 from glob2 import glob
 import torch
